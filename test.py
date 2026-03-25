d8f1e902ce9b3b325ab2750210290d8c51a012fc
Axel Nieder-Vahrenholz <a.nieder-vahrenholz@dipf.de>
2026-03-25 08:48:37 +0100

    move file_to_transform to config

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 from json2xml       import json2xml         # from: folder!
 from json2xml.utils import readfromjson
 
-file_to_test = "./examples/test.json"
+# file_to_test = "./examples/test.json"
 
 # file_to_test = "./examples/booleanjson.json"
 # file_to_test = "./examples/booleanjson2.json"
@@ -11,6 +11,8 @@ file_to_test = "./examples/test.json"
 # file_to_test = "./examples/light.json"
 # file_to_test = "./examples/wrongjson.json"
 
-data           = readfromjson(file_to_test)
+config              = readfromjson("config.json")
+file_to_transform   = config["file_to_transform"]
+data                = readfromjson(file_to_transform)
 
 print(json2xml.Json2xml(data).to_xml())
